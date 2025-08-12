eac44637027d068f1bc8df19c0f1f34dc66ee662
moazmazahid93 <moazmazahid93@gmail.com>
2025-08-12 13:35:49 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -71,13 +71,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database configuration from environment variables
 DATABASES = {
-    'default': {
+  'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'postgres',
+        'USER': 'pgadmin@snapora-db-19339',
+        'PASSWORD': 'Nine112233',
+        'HOST': 'snapora-db-19339.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
     }
 }
 
