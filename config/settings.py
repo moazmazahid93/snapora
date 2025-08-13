984f014f5a20b9475d4d33c27ac15ae2b4de0f7f
moazmazahid93 <moazmazahid93@gmail.com>
2025-08-13 14:43:35 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',  # Serve static files in production
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
